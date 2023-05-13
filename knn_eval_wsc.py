8a187f515db620efb8ab4a9d17ebb8903c109b7d
Charlotte Loh <cloh@mit.edu>
2023-05-13 13:09:23 -0400

    knn eval on new dataset

--- knn_eval_wsc.py
+++ knn_eval_wsc.py
@@ -2,18 +2,21 @@ from load_models import load_ckpt, get_dataloader, save_reps
 from ddp_utils import knn_monitor
 import torch
 
-multi_data_path='/gpfs/wscgpfs02/shivsr/cloh/spike_data/multi_dy016_random_neurons_04_28_2023'
+multi_data_path='/gpfs/wscgpfs02/shivsr/cloh/spike_data/multi_dy016_random_neurons_05_13_2023'
 
 # multi_ckpt_path = '/gpfs/wscgpfs02/shivsr/cloh/spike_contrastive/saved_models/0510_mc_conseq_causal_n64_b1331_bs120_extra5_lr0.0005_poschan/checkpoint_epoch560.pth'
 # model = load_ckpt(multi_ckpt_path, multi_chan = True, rep_after_proj=True, use_chan_pos=True)
 # model2 = load_ckpt(multi_ckpt_path, multi_chan = True, rep_after_proj=False, use_chan_pos=True)
 
 # multi_ckpt_path = '/gpfs/wscgpfs02/shivsr/cloh/spike_contrastive/saved_models/0510_mc_conseq_causal_n64_b1331_bs120_extra5_lr0.0001_poschan_half_embed/checkpoint_epoch350.pth'
-multi_ckpt_path = '/gpfs/wscgpfs02/shivsr/cloh/spike_contrastive/saved_models/0510_mc_conseq_causal_n64_b1331_bs120_extra5_lr0.0005_poschan_half_embed/checkpoint_epoch350.pth'
+# multi_ckpt_path = '/gpfs/wscgpfs02/shivsr/cloh/spike_contrastive/saved_models/0510_mc_conseq_causal_n64_b1331_bs120_extra5_lr0.0005_poschan_half_embed/checkpoint.pth'
+multi_ckpt_path = '/gpfs/wscgpfs02/shivsr/cloh/spike_contrastive/saved_models/0510_mc_conseq_causal_n64_b1331_bs120_extra5_lr0.0005_poschan_mergelayer_layernorm/checkpoint.pth'
+model = load_ckpt(multi_ckpt_path, multi_chan = True, rep_after_proj=True, use_chan_pos=True, use_merge_layer=True, add_layernorm=True, half_embed_each=False)
+model2 = load_ckpt(multi_ckpt_path, multi_chan = True, rep_after_proj=False, use_chan_pos=True, use_merge_layer=True, add_layernorm=True, half_embed_each=False)
 
 # multi_ckpt_path = '/gpfs/wscgpfs02/shivsr/cloh/spike_contrastive/saved_models/0510_mc_conseq_causal_n64_b1331_bs120_extra5_lr0.0001_poschan_half_embed/checkpoint_epoch350.pth'
-model = load_ckpt(multi_ckpt_path, multi_chan = True, rep_after_proj=True, use_chan_pos=True, use_merge_layer=False, add_layernorm=False, half_embed_each=True)
-model2 = load_ckpt(multi_ckpt_path, multi_chan = True, rep_after_proj=False, use_chan_pos=True, use_merge_layer=False, add_layernorm=False, half_embed_each=True)
+# model = load_ckpt(multi_ckpt_path, multi_chan = True, rep_after_proj=True, use_chan_pos=True, use_merge_layer=False, add_layernorm=False, half_embed_each=True)
+# model2 = load_ckpt(multi_ckpt_path, multi_chan = True, rep_after_proj=False, use_chan_pos=True, use_merge_layer=False, add_layernorm=False, half_embed_each=True)
 
 train_loader = get_dataloader(multi_data_path, multi_chan=True, split='train', use_chan_pos=True)
 test_loader = get_dataloader(multi_data_path, multi_chan=True, split='test', use_chan_pos=True)
@@ -24,17 +27,16 @@ class Args:
     num_extra_chans: int = 5
 args = Args()
 
-save_reps(model.cuda(), test_loader, multi_ckpt_path, split='test', multi_chan=True, rep_after_proj=True, use_chan_pos=True)
-save_reps(model.cuda(), train_loader, multi_ckpt_path, split='train', multi_chan=True, rep_after_proj=True, use_chan_pos=True)
-
-
-knn_score = knn_monitor(net=model.cuda(), memory_data_loader=train_loader, test_data_loader=test_loader, device='cuda',k=200, hide_progress=True, args=args)
-print("knn if rep AFTER proj:", knn_score)
-
 
 save_reps(model2.cuda(), test_loader, multi_ckpt_path, split='test', multi_chan=True, rep_after_proj=False, use_chan_pos=True)
+save_reps(model.cuda(), test_loader, multi_ckpt_path, split='test', multi_chan=True, rep_after_proj=True, use_chan_pos=True)
+
 save_reps(model2.cuda(), train_loader, multi_ckpt_path, split='train', multi_chan=True, rep_after_proj=False, use_chan_pos=True)
 
+save_reps(model.cuda(), train_loader, multi_ckpt_path, split='train', multi_chan=True, rep_after_proj=True, use_chan_pos=True)
 
 knn_score = knn_monitor(net=model2.cuda(), memory_data_loader=train_loader, test_data_loader=test_loader, device='cuda',k=200, hide_progress=True, args=args)
 print("knn if rep BEFORE proj:", knn_score)
+
+knn_score = knn_monitor(net=model.cuda(), memory_data_loader=train_loader, test_data_loader=test_loader, device='cuda',k=200, hide_progress=True, args=args)
+print("knn if rep AFTER proj:", knn_score)
